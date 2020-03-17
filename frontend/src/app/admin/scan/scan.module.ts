7af1ff298bf3bce241fe598d66ca22f13c8fa6d1
Michal Kormos <Michal.Kormos@iblsoft.com>
2020-03-17 17:52:53 +0100

    Empty page for scanning

---
+++ frontend/src/app/admin/scan/scan.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { SCAN_ROUTE } from './scan.route';
+import { ScanComponent } from './scan.component';
+
+@NgModule({
+  imports: [RouterModule.forChild([SCAN_ROUTE])],
+  declarations: [ScanComponent]
+})
+export class ScanModule {}
---
+++ frontend/src/app/admin/scan/scan.route.ts
@@ -0,0 +1,12 @@
+import { Route } from '@angular/router';
+
+import { ScanComponent } from './scan.component';
+
+export const SCAN_ROUTE: Route = {
+  path: 'admin/scan',
+  component: ScanComponent,
+  data: {
+    authorities: [],
+    pageTitle: 'Welcome'
+  }
+};
