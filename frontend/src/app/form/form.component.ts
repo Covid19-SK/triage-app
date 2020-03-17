816ced24aa60222369f970664c7b9597ed613c8b
Michal Kormos <Michal.Kormos@iblsoft.com>
2020-03-17 08:22:17 +0100

    Examination form

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {first, map, shareReplay} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {ExamService} from '../shared/exam.service';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +10,33 @@ import {Component} from '@angular/core';
   styleUrls: ['form.scss']
 })
 export class FormComponent {
-  constructor() {}
+  public form$: Observable<FormGroup>;
+
+  constructor(private examService: ExamService) {
+    this.form$ = this.examService.exam$.pipe(
+      map(exam => new FormGroup({
+          place: new FormControl(exam.place),
+          cough: new FormControl(exam.cough),
+          breathShortness: new FormControl(exam.breathShortness),
+          fever: new FormControl(exam.fever),
+          other: new FormControl(exam.other),
+        })
+      ),
+      shareReplay(1)
+    );
+  }
+
+  public onSubmit(): void {
+    this.form$.pipe(first()).subscribe(
+      form => {
+        this.examService.setExam({
+          place: form.value['place'],
+          cough: form.value['cough'],
+          breathShortness: form.value['breathShortness'],
+          fever: form.value['fever'],
+          other: form.value['other'],
+        });
+      }
+    );
+  }
 }
