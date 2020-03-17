9253fc71213859298b45895a76870847d8f0e106
Franto Kormanak <Franto.Kormanak@iblsoft.com>
2020-03-17 22:19:10 +0100

    Show QR Code after Form

--- frontend/src/app/code/code.module.ts
+++ frontend/src/app/code/code.module.ts
@@ -2,9 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {CODE_ROUTE} from './code.route';
 import {CodeComponent} from './code.component';
+import {QRCodeModule} from 'angular2-qrcode';
 
 @NgModule({
-  imports: [RouterModule.forChild([CODE_ROUTE])],
+  imports: [
+    QRCodeModule,
+    RouterModule.forChild([CODE_ROUTE])],
   declarations: [CodeComponent]
 })
 export class CodeModule {
