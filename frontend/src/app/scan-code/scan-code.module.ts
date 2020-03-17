90d653092127dda9c732bca8221527a9d93915e8
Franto Kormanak <Franto.Kormanak@iblsoft.com>
2020-03-17 23:37:03 +0100

    scan code

--- frontend/src/app/scan-code/scan-code.module.ts
+++ frontend/src/app/scan-code/scan-code.module.ts
@@ -3,9 +3,11 @@ import {RouterModule} from '@angular/router';
 import {SCAN_CODE_ROUTE} from './scan-code.route';
 import {ScanCodeComponent} from './scan-code.component';
 import {NgxQRCodeModule} from 'ngx-qrcode2';
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   imports: [
+    BrowserModule,
     NgxQRCodeModule,
     RouterModule.forChild([SCAN_CODE_ROUTE])],
   declarations: [ScanCodeComponent]
