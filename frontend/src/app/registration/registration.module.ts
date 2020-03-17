74b5f6a807662dd412aeab59837a409c4c60c938
Franto Kormanak <Franto.Kormanak@iblsoft.com>
2020-03-17 17:42:53 +0100

    Registration form as multi step form (without validation)

--- frontend/src/app/registration/registration.module.ts
+++ frontend/src/app/registration/registration.module.ts
@@ -3,15 +3,22 @@ import {RouterModule} from '@angular/router';
 
 import {REGISTRATION_ROUTE} from './registration.route';
 import {RegistrationComponent} from './registration.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
+import {MultiStepFormComponent} from '../multi-step-form/multi-step-form.component';
+import {FormStepDirective} from '../directives/form-step.directive';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild([REGISTRATION_ROUTE])
   ],
-  declarations: [RegistrationComponent]
+  declarations: [
+    RegistrationComponent,
+    MultiStepFormComponent,
+    FormStepDirective
+  ]
 })
 export class RegistrationModule {}
