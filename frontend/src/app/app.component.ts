926fc23ea02758cd72975b3e33241003f626976e
gabrielnemeth <gabrielnemeth@hotmail.sk>
2020-03-17 16:29:50 +0100

    Add head component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  public title = 'frontend';
+  public icon = faInfoCircle;
 }
