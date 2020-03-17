be6c796af505fec160c605e602c7141abfa804f4
Michal Kormos <Michal.Kormos@iblsoft.com>
2020-03-17 14:41:18 +0100

    Exam page

--- frontend/src/app/admin/patients/exam/exam.component.ts
+++ frontend/src/app/admin/patients/exam/exam.component.ts
@@ -1,4 +1,11 @@
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Exam} from '../../../shared/exam';
+import {switchMap} from 'rxjs/operators';
+import {ExamService} from '../../../shared/exam.service';
+import {ActivatedRoute} from '@angular/router';
+import {Patient} from '../../../shared/patient';
+import {PatientsService} from '../../../shared/patients.service';
 
 @Component({
   selector: 'app-exam',
@@ -6,5 +13,15 @@ import {Component} from '@angular/core';
   styleUrls: ['exam.scss']
 })
 export class ExamComponent {
-  constructor() {}
+  public exam$: Observable<Exam> = this.route.paramMap.pipe(
+    switchMap(paramMap => this.examService.getExam(paramMap.get('examId')))
+  );
+  public patient$: Observable<Patient> = this.exam$.pipe(
+    switchMap(exam => this.patientsService.getById(exam.patientId))
+  );
+  constructor(
+    private patientsService: PatientsService,
+    private examService: ExamService,
+    private route: ActivatedRoute
+  ) {}
 }
