ae8eec762a122076b129d899e206dae62e2a86f2
Franto Kormanak <Franto.Kormanak@iblsoft.com>
2020-03-17 19:57:18 +0100

    Exam component as multi step form

---
+++ frontend/src/app/form-components.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {FormStepDirective} from './directives/form-step.directive';
+import {MultiStepFormComponent} from './multi-step-form/multi-step-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+    MultiStepFormComponent,
+    FormStepDirective
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MultiStepFormComponent,
+    FormStepDirective
+  ]
+})
+export class FormComponentsModule {}
