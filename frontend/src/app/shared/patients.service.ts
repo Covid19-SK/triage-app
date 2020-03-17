8187bda9c8993316a877df53c1a66aaebb4a0ef5
Michal Kormos <Michal.Kormos@iblsoft.com>
2020-03-17 14:41:18 +0100

    Exam page

--- frontend/src/app/shared/patients.service.ts
+++ frontend/src/app/shared/patients.service.ts
@@ -1,42 +1,56 @@
 import {Injectable} from '@angular/core';
 import {Patient} from './patient';
-import {Observable, Subject} from 'rxjs';
+import {Observable, of, Subject} from 'rxjs';
 import {shareReplay, startWith} from 'rxjs/operators';
 import {DataService} from './data.service';
+import {first} from 'lodash-es';
+
+const defaultPatients = [
+  {
+    id: 'vncnt',
+    firstName: 'Vincent',
+    lastName: 'Cierny',
+    birthId: '9902029184',
+    email: 'vincent.cierny@email.com',
+    phone: '+421 902 029 184',
+  },
+  {
+    id: 'rchrd',
+    firstName: 'Richard',
+    lastName: 'Kuchar',
+    birthId: '9905105392',
+    email: 'richard.kuchar@email.com',
+    phone: '+421 905 105 392',
+  },
+  {
+    id: 'brbr',
+    firstName: 'Barbora',
+    lastName: 'Janosikova',
+    birthId: '9901332950',
+    email: 'barbora.janosikova@email.com',
+    phone: '+421 901 332 950',
+  },
+];
 
 @Injectable({providedIn: 'root'})
 export class PatientsService {
   private patientsSource$: Subject<Patient[]> = new Subject();
   public patients$: Observable<Patient[]> = this.patientsSource$.asObservable().pipe(
-    startWith(this.dataService.load('tpPatients', [
-      {
-        id: 'vncnt',
-        firstName: 'Vincent',
-        lastName: 'Cierny',
-        birthId: '9902029184',
-        email: 'vincent.cierny@email.com',
-        phone: '+421 902 029 184',
-      },
-      {
-        id: 'rchrd',
-        firstName: 'Richard',
-        lastName: 'Kuchar',
-        birthId: '9905105392',
-        email: 'richard.kuchar@email.com',
-        phone: '+421 905 105 392',
-      },
-      {
-        id: 'brbr',
-        firstName: 'Barbora',
-        lastName: 'Janosikova',
-        birthId: '9901332950',
-        email: 'barbora.janosikova@email.com',
-        phone: '+421 901 332 950',
-      },
-    ])),
+    startWith(this.getAll()),
     shareReplay(1)
   );
 
   public constructor(private dataService: DataService) {
+  }
+
+  private getAll(): Patient[] {
+    return this.dataService.load('tpPatients', defaultPatients);
+  }
+
+  public getById(patientId: string): Observable<Patient> {
+    const patients = this.getAll()
+      .filter(p => p.id === patientId);
+    console.assert(patients.length === 1);
+    return of(first(patients));
   }
 }
