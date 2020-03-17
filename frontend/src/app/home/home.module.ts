87846c4c4984e4369c831c7b429fbce5f7172463
gabrielnemeth <gabrielnemeth@hotmail.sk>
2020-03-17 20:02:01 +0100

    Style home page

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { RouterModule } from '@angular/router';
 
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
-  imports: [RouterModule.forChild([HOME_ROUTE])],
+  imports: [RouterModule.forChild([HOME_ROUTE]), CommonModule],
   declarations: [HomeComponent]
 })
 export class HomeModule {}
