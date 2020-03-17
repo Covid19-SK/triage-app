ab958e16c3d087cefdacbb3ed60ec563e0e1b575
Franto Kormanak <Franto.Kormanak@iblsoft.com>
2020-03-17 19:57:18 +0100

    Exam component as multi step form

--- frontend/src/app/form/form.module.ts
+++ frontend/src/app/form/form.module.ts
@@ -4,12 +4,12 @@ import { RouterModule } from '@angular/router';
 import { FORM_ROUTE } from './form.route';
 import { FormComponent } from './form.component';
 import {CommonModule} from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormComponentsModule} from '../form-components.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule,
+    FormComponentsModule,
     RouterModule.forChild([FORM_ROUTE])
   ],
   declarations: [FormComponent]
