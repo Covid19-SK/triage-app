b6d45e6fa5be037787aa104f2632bd0a46bb9dab
Franto Kormanak <Franto.Kormanak@iblsoft.com>
2020-03-17 19:57:18 +0100

    Exam component as multi step form

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -12,39 +12,57 @@ import {InstitutionsService} from '../shared/institutions.service';
   styleUrls: ['form.scss']
 })
 export class FormComponent {
-  public form$: Observable<FormGroup>;
-  public institutions$: Observable<Institution[]> = this.institutionsService.institutions$;
+  public form$: Observable<FormGroup[]>;
 
   constructor(
     private examService: ExamService,
     private institutionsService: InstitutionsService
   ) {
     this.form$ = this.examService.exam$.pipe(
-      map(exam => new FormGroup({
-          institution: new FormControl(exam.institution),
-          cough: new FormControl(exam.cough),
-          breathShortness: new FormControl(exam.breathShortness),
-          fever: new FormControl(exam.fever),
-          other: new FormControl(exam.other),
+      map(exam => [
+        new FormGroup({
+          workplace: new FormControl(exam.workplace)
+        }),
+        new FormGroup({
+          cough: new FormControl(exam.cough)
+        }),
+        new FormGroup({
+          breathShortness: new FormControl(exam.breathShortness)
+        }),
+        new FormGroup({
+          fever: new FormControl(exam.fever)
+        }),
+        new FormGroup({
+          other: new FormControl(exam.other)
         })
-      ),
+      ]),
       shareReplay(1)
+    );
+  }
+
+  public onNextAction(): void {
+    this.form$.pipe(first()).subscribe(
+      form => this.updateExam(form)
     );
   }
 
   public onSubmit(): void {
     this.form$.pipe(first()).subscribe(
-      form => {
-        this.examService.setExam({
-          // TODO: use something normal
-          date: new Date().toString(),
-          institution: form.value['institution'],
-          cough: form.value['cough'],
-          breathShortness: form.value['breathShortness'],
-          fever: form.value['fever'],
-          other: form.value['other'],
-        });
-      }
+      form => this.updateExam(form)
     );
+  }
+
+  private updateExam(form: FormGroup[]): void {
+    const exam = {
+      // TODO: use something normal
+      date: new Date().toString(),
+      workplace: form[0].controls['workplace'].value,
+      cough: form[1].controls['cough'].value,
+      breathShortness: form[2].controls['breathShortness'].value,
+      fever: form[3].controls['fever'].value,
+      other: form[4].controls['other'].value,
+    };
+    this.examService.setExam(exam);
+
   }
 }
--- frontend/src/app/registration/registration.module.ts
+++ frontend/src/app/registration/registration.module.ts
@@ -3,22 +3,17 @@ import {RouterModule} from '@angular/router';
 
 import {REGISTRATION_ROUTE} from './registration.route';
 import {RegistrationComponent} from './registration.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {MultiStepFormComponent} from '../multi-step-form/multi-step-form.component';
-import {FormStepDirective} from '../directives/form-step.directive';
+import {FormComponentsModule} from '../form-components.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
+    FormComponentsModule,
     RouterModule.forChild([REGISTRATION_ROUTE])
   ],
   declarations: [
     RegistrationComponent,
-    MultiStepFormComponent,
-    FormStepDirective
   ]
 })
 export class RegistrationModule {}
