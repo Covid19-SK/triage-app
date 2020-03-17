e0a939be6d82861f3bfd3accc7473acd81fea5e0
Matej Svec <matej@msvec.sk>
2020-03-17 13:24:49 +0100

    Add patients

---
+++ backend/migrations/20200317100202-create-patient.js
@@ -0,0 +1,55 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('patients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      identificationNumber: {
+        type: Sequelize.STRING,
+      },
+      dateOfBirth: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      phoneNumber: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      town: {
+        type: Sequelize.STRING,
+      },
+      zipCode: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('patients');
+  },
+};
---
+++ backend/src/patients/patients.service.ts
@@ -0,0 +1,66 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { PatientDto } from 'src/patients/patient.dto';
+import { Patient } from './patient.model';
+
+@Injectable()
+export class PatientsService {
+  constructor(@InjectModel(Patient) private patient: typeof Patient) {}
+
+  async findAll(): Promise<PatientDto[]> {
+    const patients = this.patient.findAll();
+    return patients.map(p => new PatientDto(p));
+  }
+
+  async find(id: string): Promise<PatientDto> {
+    const patient = await this.patient.findByPk(id);
+    if (!patient) {
+      throw new HttpException('No patient found.', HttpStatus.NOT_FOUND);
+    }
+    return new PatientDto(patient);
+  }
+
+  async create(dto: PatientDto): Promise<Patient> {
+    const patient = new Patient();
+    patient.firstName = dto.firstName;
+    patient.lastName = dto.lastName;
+    patient.dateOfBirth = dto.dateOfBirth;
+    patient.phoneNumber = dto.phoneNumber;
+    patient.identificationNumber = dto.identificationNumber;
+    patient.email = dto.email;
+    patient.address = dto.address;
+    patient.town = dto.town;
+    patient.zipCode = dto.zipCode;
+    return patient.save();
+  }
+
+  async update(id: string, dto: PatientDto): Promise<PatientDto> {
+    const patient = await this.patient.findByPk(id);
+    if (!patient) {
+      throw new HttpException('No patient found.', HttpStatus.NOT_FOUND);
+    }
+    patient.firstName = dto.firstName || patient.firstName;
+    patient.lastName = dto.lastName || patient.lastName;
+    patient.dateOfBirth = dto.dateOfBirth || patient.dateOfBirth;
+    patient.phoneNumber = dto.phoneNumber || patient.phoneNumber;
+    patient.identificationNumber =
+      dto.identificationNumber || patient.identificationNumber;
+    patient.email = dto.email || patient.email;
+    patient.address = dto.address || patient.address;
+    patient.town = dto.town || patient.town;
+    patient.zipCode = dto.zipCode || patient.zipCode;
+
+    try {
+      const patient2 = await patient.save();
+      return new PatientDto(patient2);
+    } catch (err) {
+      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async delete(id: string): Promise<PatientDto> {
+    const patient = await this.patient.findByPk(id);
+    await patient.destroy();
+    return new PatientDto(patient);
+  }
+}
