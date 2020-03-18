56257db5dea2da690d418777fc185f69b0783efc
gabrielnemeth <gabrielnemeth@hotmail.sk>
2020-03-18 14:22:33 +0100

    Style scan qr code page

--- frontend/src/app/scan-code/scan-code.module.ts
+++ frontend/src/app/scan-code/scan-code.module.ts
@@ -5,6 +5,8 @@ import { ScanCodeComponent } from './scan-code.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { WebcamPreviewDirective } from './webcam-preview.directive';
 import { ModalModule, BsDropdownModule, ButtonsModule } from 'ngx-bootstrap';
+import {SharedModule} from '../shared/shared.module';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { ModalModule, BsDropdownModule, ButtonsModule } from 'ngx-bootstrap';
     ModalModule.forRoot(),
     ButtonsModule.forRoot(),
     BsDropdownModule.forRoot(),
-    RouterModule.forChild([SCAN_CODE_ROUTE])
+    RouterModule.forChild([SCAN_CODE_ROUTE]),
+    SharedModule,
+    FontAwesomeModule
   ],
   declarations: [ScanCodeComponent, WebcamPreviewDirective]
 })
