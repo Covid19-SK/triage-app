42a76eabdf77771e5da8f97ec41e3a6ed0790638
Franto Kormanak <Franto.Kormanak@iblsoft.com>
2020-03-18 13:02:45 +0100

    Add TranslateModule to SharedModule

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,18 +2,37 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {HeaderComponent} from '../widgets/header/header.component';
+import {TranslateLoader, TranslateModule, TranslateStore} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    })
   ],
   declarations: [
     HeaderComponent,
   ],
   exports: [
-    HeaderComponent
-  ]
+    HeaderComponent,
+    TranslateModule
+  ],
+  providers: [TranslateStore]
 })
 export class SharedModule {
 }
