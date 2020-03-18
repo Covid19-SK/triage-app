65c5604e386f1c0fff9f34fc3647cbc7368d71d5
g43riko <gabrielcsollei@gmail.com>
2020-03-18 09:13:33 +0100

    Add QR code reader

---
+++ frontend/src/app/scan-code/webcam-preview.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, EventEmitter, OnDestroy, Output } from '@angular/core';
+import { Camera, ScanCodeService } from './scan-code.service';
+
+@Directive({
+  selector: 'video[appWebcamPreview]',
+  exportAs: 'webCamPreview'
+})
+export class WebcamPreviewDirective implements OnDestroy {
+  @Output() public readonly codeScanned: EventEmitter<string> = new EventEmitter();
+  public constructor(private readonly elementRef: ElementRef<HTMLVideoElement>,
+                     private readonly scanCodeService: ScanCodeService) {
+    this.scanCodeService.scanCodeCam(this.elementRef.nativeElement).subscribe((result) => {
+      this.codeScanned.next(result);
+    });
+  }
+  public useCamera(camera: Camera): void {
+    this.scanCodeService.stopScanning();
+    this.scanCodeService.scanUsingWebCam(this.elementRef.nativeElement, camera).subscribe((result) => {
+      this.codeScanned.next(result);
+    });
+  }
+  public ngOnDestroy(): void {
+    this.scanCodeService.stopScanning();
+  }
+}
