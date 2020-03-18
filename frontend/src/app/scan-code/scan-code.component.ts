8780cebb1d869a1b168a7db94cc81d67e55db984
gabrielnemeth <gabrielnemeth@hotmail.sk>
2020-03-18 14:22:33 +0100

    Style scan qr code page

--- frontend/src/app/scan-code/scan-code.component.ts
+++ frontend/src/app/scan-code/scan-code.component.ts
@@ -1,24 +1,28 @@
-import { Component, TemplateRef } from '@angular/core';
-import { AuthService } from '../shared/auth.service';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
-import { ScanCodeService } from './scan-code.service';
+import {Component, TemplateRef} from '@angular/core';
+import {AuthService} from '../shared/auth.service';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
+import {ScanCodeService} from './scan-code.service';
+import {faChevronLeft, faQrcode} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector   : 'app-code',
+  selector: 'app-code',
   templateUrl: './scan-code.component.html',
-  styleUrls  : ['scan-code.scss']
+  styleUrls: ['scan-code.scss']
 })
 export class ScanCodeComponent {
-  public userId: string                        = this.authService.id;
+  public userId: string = this.authService.id;
   public elementType: 'url' | 'canvas' | 'img' = 'url';
-  public value                                 = 'Techiediaries';
+  public value = 'Techiediaries';
   public code: string;
   private modalRef: BsModalRef;
+  public icon = faQrcode;
+  public backIcon = faChevronLeft;
 
   public constructor(private readonly authService: AuthService,
                      public readonly scanCodeService: ScanCodeService,
                      private modalService: BsModalService) {
   }
+
   public openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
